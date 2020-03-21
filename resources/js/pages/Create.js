d970859b47e9151dc82716478a11e79f0741d430
nzbenji <benlittle53@gmail.com>
2020-03-21 18:11:55 +1300

    rewrote create component to hooks

--- resources/js/pages/Create.js
+++ resources/js/pages/Create.js
@@ -1,111 +1,40 @@
-import React, { Component } from "react";
+import React from 'react';
 import { connect } from "react-redux";
 import Http from "../Http";
+import { useForm } from 'react-hook-form';
 
-class Dashboard extends Component {
-    constructor(props) {
-        super(props);
+function Create() {
+    const url = 'http://laravel-react-bootstrap.test/api/project';
 
-        // Initial state.
-        this.state = {
-            project: null,
-            todo: null,
-            error: false,
-            data: [],
-            projectData: []
-        };
-
-        // API endpoint.
-        this.api = "/api/project";
-    }
-
-
-    handleChange = e => {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
+    const { register, handleSubmit, errors } = useForm(); // initialise the hook
+    const onSubmit = ({title, description}) => {
+        addProject(title, description)
     };
 
-    handleSubmit = e => {
-        e.preventDefault();
-        const { todo } = this.state;
-        this.addTodo(todo);
-    };
+    async function addProject(title, description) {
+        const sendProject = Http.post(url, { title, description })
 
-    addTodo = todo => {
-        console.log(todo)
-        Http.post(this.api, { title: todo, description: 'gfggfddgf'})
-            .then(({ data }) => {
-                console.log(data)
-            })
+        const { data } = await sendProject
+        console.log(data)
+
+        const newItem = {
+            id: data.id,
+            title,
+            description
         }
-
-
-    render() {
-        const { data, error } = this.state;
-        return (
-            <div className="container py-5">
-                <div className="add-todos mb-5">
-                    <h1 className="text-center mb-4">Add a To Do</h1>
-                    <form
-                        method="post"
-                        onSubmit={this.handleSubmit}
-                        ref={el => {
-                            this.todoForm = el;
-                        }}
-                    >
-                        <div className="form-group">
-                            <label htmlFor="addTodo">Add a New To Do</label>
-                            <div className="d-flex">
-                                <input
-                                    id="addTodo"
-                                    name="todo"
-                                    className="form-control mr-3"
-                                    placeholder="Build a To Do app..."
-                                    onChange={this.handleChange}
-                                />
-                                <button type="submit" className="btn btn-primary">
-                                    Add
-                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
-                {error && (
-                    <div className="alert alert-warning" role="alert">
-                        {error}
-                    </div>
-                )}
-
-                <div className="todos">
-                    <h1 className="text-center mb-4">Open To Dos</h1>
-                    <table className="table table-striped">
-                        <tbody>
-                            <tr>
-                                <th>To Do</th>
-                                <th>Action</th>
-                            </tr>
-                            {data.map(todo => (
-                                <tr key={todo.id}>
-                                    <td>{todo.value}</td>
-                                    <td>
-                                        <button
-                                            type="button"
-                                            className="btn btn-secondary"
-                                            onClick={this.closeTodo}
-                                            data-key={todo.id}
-                                        >
-                                            Close
-                    </button>
-                                    </td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
+        console.log(newItem)
     }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input name="title" ref={register({ required: true })} />
+            {errors.title && 'Title is required.'}
+
+            <input name="description" ref={register({ required: true })} />
+            {errors.age && 'Description is required.'}
+
+            <input type="submit" />
+        </form>
+    );
 }
 
 const mapStateToProps = state => ({
@@ -113,4 +42,4 @@ const mapStateToProps = state => ({
     user: state.Auth.user
 });
 
-export default connect(mapStateToProps)(Dashboard);
+export default connect(mapStateToProps)(Create);
